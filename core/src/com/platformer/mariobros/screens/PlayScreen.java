70eee8d0721951e0ed2baa0a6a396f3847322742
gabrielstrzal <gabrielstrzal@gmail.com>
2017-08-26 11:35:11 -0300

    set aspect ratio

--- core/src/com/platformer/mariobros/screens/PlayScreen.java
+++ core/src/com/platformer/mariobros/screens/PlayScreen.java
@@ -3,7 +3,12 @@ package com.platformer.mariobros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.platformer.mariobros.MarioBros;
 
 /**
@@ -13,10 +18,14 @@ import com.platformer.mariobros.MarioBros;
 public class PlayScreen implements Screen{
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game){
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gamecam);
     }
 
     @Override
@@ -28,6 +37,7 @@ public class PlayScreen implements Screen{
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
         game.batch.draw(texture,0,0);
         game.batch.end();
@@ -35,7 +45,7 @@ public class PlayScreen implements Screen{
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
